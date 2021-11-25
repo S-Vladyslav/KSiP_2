09e853b9f5a31f1b13fc6bbde4addf58232d8f3a
Владислав Валерійович <vladyslavsyrotyuk@gmail.com>
2021-11-25 03:11:45 +0200

    Kasiski don't work :(

--- KSiP2/Program.cs
+++ KSiP2/Program.cs
@@ -6,22 +6,31 @@ namespace KSiP2
 {
     class Program
     {
-        static string Alphabet = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгґдеєжзиіїйклмнопрстуфхцчшщьюя"; //your alphabet
+        static readonly string Alphabet = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ";//абвгґдеєжзиіїйклмнопрстуфхцчшщьюя"; //your alphabet
 
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-            var rawText = Text.RawText;
+            var rawText = Text.RawText.ToUpper();
             
-            var key = "Жидачівщина";
+            var key = "АБВГҐ";
 
-            //var encryptedText = Encryption(rawText, key);
+            var encryptedText = Encryption(rawText, key);
 
-            //Console.WriteLine(encryptedText + "\n\n");
-           // Console.WriteLine(Decryption(encryptedText, key));
-            Console.WriteLine(Kasiski(rawText));
+            Console.WriteLine(encryptedText + "\n\n");
+            Console.WriteLine(Decryption(encryptedText, key));
 
+            var result = Kasiski(encryptedText);
+
+            Console.WriteLine("\n\nНайймовірніші довжини ключів:");
+            foreach (var i in result)
+            {
+                if (i > 1) Console.WriteLine(i);
+            }
+
+            Console.WriteLine($"Довжина справжнього ключа: {key.Length}");
+            Console.WriteLine($"Довжина тексту: {encryptedText.Length}");
         }
 
         #region Encryption
@@ -94,12 +103,12 @@ namespace KSiP2
         #endregion
 
         #region Kasiski
-        static string Kasiski(string startText)
+        static List<int> Kasiski(string startText)
         {
             var setsDictionary = new Dictionary<string, List<int>>();
-            
-            var setFromText = "";
+
             var set = "";
+
             var encryptedText = "";
 
             foreach (var symbol in startText)
@@ -131,18 +140,70 @@ namespace KSiP2
                 }
             }
 
-            foreach (var item in setsDictionary)
+            var mostCommon = setsDictionary.Where(el => el.Value.Count > 2);
+
+            //foreach (var i in mostCommon)
+            //{
+            //    Console.WriteLine(i.Value.Count);
+            //}
+
+            var distList = new List<List<int>>();
+
+            foreach (var coordinates in mostCommon)
             {
-                if (item.Value.Count > 1) Console.WriteLine($"{item.Key} : {item.Value.Count}");
+                List<int> coord = coordinates.Value.ToList();
+
+                var differense = new List<int>();
+
+                for (int i = 1; i < coord.Count; i++)
+                {
+                    differense.Add(coord[i] - coord[i - 1]);
+                }
+
+                distList.Add(differense);
             }
 
-            #region Linq
-            var mostCommon = setsDictionary.OrderByDescending(el => el.Value.Count > 2);
+            var GCD = new List<int>();
 
-            Console.WriteLine("");
-            //return mostCommon.ToString();
-            #endregion
-            return "";
+            foreach (var j in distList)
+            {
+                //var listGCD = j[0];
+                for (int i = 1; i < j.Count; i++)
+                {
+                    Console.WriteLine($"i = {j[i]}\t i-1 = {j[i - 1]}");
+                    //listGCD = GCDMethod(listGCD, j[i]);
+                    GCD.Add(GCDMethod(j[i-1], j[i]));
+                }
+                //GCD.Add(listGCD);
+            }
+
+            return GCD.Distinct().ToList();
+        }
+        #endregion
+
+        #region GCDMethod
+        static int GCDMethod(int a, int b)
+        {
+            if (a == 0)
+            {
+                return b;
+            }
+            else
+            {
+                var min = Min(a, b);
+                var max = Max(a, b);
+                //викликаємо метод з новими аргументами
+                return GCDMethod(max - min, min);
+            }
+        }
+        static int Min(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+
+        static int Max(int x, int y)
+        {
+            return x > y ? x : y;
         }
         #endregion
     }
